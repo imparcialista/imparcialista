fe5c40c981800682d20dfb893b05290c59d3b0aa
imparcialista <lucasaraujoz13@outlook.com>
2024-01-15 10:03:22 -0300

    update

--- python/programas/meta/mk_folder.py
+++ python/programas/meta/mk_folder.py
@@ -5,13 +5,11 @@ lista_de_skus = []
 descricao_dos_produtos = []
 preco_dos_produtos = []
 lista = []
-# list_geral = []
 
-gui.PAUSE = 0.10
+gui.PAUSE = 0.15
 
 # monitor 1360 x 768
 clicar_na_imagem = 678, 422
-
 selecionar_troca_imagem = 1143, 545
 selecionar_troca_imagem_catalogo = 1142, 451
 clicar_subs_imagem = 1000, 432
@@ -28,15 +26,15 @@ def tratrar_sku(codigo_sku):
 pausa(7,
       'Clique em prosseguir, e em seguida clique na primeira linha para copiar')
 
-valor_de_linhas = gui.prompt(text='Qual a quantidade de linhas para serem copiadas?',
-                             title='Quantidade de linhas',
-                             default='16')
+valor_de_linhas = entrada_usuario('Qual a quantidade de linhas para serem copiadas?',
+                'Quantidade de linhas',
+                '16')
 
 linhas_para_copiar = int(valor_de_linhas)
 
-copiar_preco = gui.confirm(f'Copiar coluna preço?',
-                           'Coluna preço',
-                           buttons=['Sim', 'Não'])
+copiar_preco = confirmar('Copiar coluna de preço?',
+                         'Coluna preço',
+                         'Sim', 'Não')
 
 pausa(5,
       'Deseja pausar o programa?')
@@ -58,15 +56,17 @@ for i in range(linhas_para_copiar):
     gui.press('home')
     gui.press('down')
 
-itens_por_linha = gui.prompt(text='São quantos itens por linha?',
-                             title='Quantidade de itens por linha',
-                             default='4')
 
+itens_por_linha = entrada_usuario('São quantos itens por linha?',
+                                  'Quantidade de itens por linha',
+                                  '4')
 
 itens_por_linha = int(itens_por_linha)
+
 print('--------Descrição---------')
 pausa(5,
       'Coloque o programa no figma e aperte enter no texto para entrar na edição')
+
 
 def contagem(valor):
     for indice in range(valor, (valor + 4)):
@@ -85,6 +85,7 @@ def contagem(valor):
         gui.hotkey('ctrl', 'v')
         tab()
 
+
 def catalogo():
     for indice in range(linhas_para_copiar):
         codigo = str(descricao_dos_produtos[indice])
@@ -99,28 +100,31 @@ def catalogo():
         gui.hotkey('ctrl', 'v')
         tab()
 
+
 for item in lista_de_skus:
     tratrar_sku(item)
 
-'''
+# '''
 contagem(0)
 contagem(4)
 contagem(8)
 contagem(12)
-'''
-catalogo()
+# '''
+
+# catalogo()
+
 
 def trocar_a_foto(codigo_sku):
     gui.PAUSE = 1
-    clicar(selecionar_troca_imagem_catalogo)
-    clicar(clicar_subs_imagem_catalogo)
-    time.sleep(0.5)
+    clicar(selecionar_troca_imagem)
+    clicar(clicar_subs_imagem)
+    time.sleep(1)
     ir_para_pasta(clicar_caminho_da_pasta, pasta_com_fotos)
-    time.sleep(0.5)
+    time.sleep(1)
     clicar(clicar_nome_do_arquivo)
-    time.sleep(0.5)
+    time.sleep(1)
     gui.write(f'{codigo_sku}.png')
-    time.sleep(0.25)
+    time.sleep(1)
     gui.hotkey('alt', 'a')
     print(f'{codigo_sku} feito')
     time.sleep(1)
@@ -129,23 +133,24 @@ def trocar_a_foto(codigo_sku):
     tab()
     time.sleep(0.5)
 
+
 def trocar_a_foto_catalogo(codigo_sku):
     gui.PAUSE = 1
     clicar(selecionar_troca_imagem_catalogo)
     clicar(clicar_subs_imagem_catalogo)
-    time.sleep(0.5)
+    time.sleep(1)
     ir_para_pasta(clicar_caminho_da_pasta, pasta_com_fotos)
-    time.sleep(0.5)
+    time.sleep(1)
     clicar(clicar_nome_do_arquivo)
-    time.sleep(0.5)
+    time.sleep(1)
     gui.write(f'{codigo_sku}.png')
-    time.sleep(0.25)
+    time.sleep(1)
     gui.hotkey('alt', 'a')
     print(f'{codigo_sku} feito')
     time.sleep(1)
     esc()
     time.sleep(2)
-    # tab()
+
 
 pausa(1,
       'Selecione a pasta com as imagens sem fundo')
@@ -156,47 +161,46 @@ print('--------Imagens---------')
 pausa(5,
       'Clique na primeira imagem no figma para a imagem ser alterada')
 
-# for item_da_lista in lista:
-    # trocar_a_foto(item_da_lista)
-    # contador += 1
-
+for item_da_lista in lista:
+    trocar_a_foto(item_da_lista)
 
 pos_x = 520
 pos_y = 210
 
 posicao_imagem = pos_x, pos_y
 
+'''
+# use esse for para trocar as fotos do catalogo
 for item_da_lista in lista:
     if pos_x > 840:
         pos_y += 130
         pos_x = 520
         posicao_imagem = pos_x, pos_y
         clicar(posicao_imagem)
-        esperar(0.5)
+        time.sleep(1)
         clicar_duas_vezes(posicao_imagem)
-        esperar(0.5)
+        time.sleep(1)
         clicar_duas_vezes(posicao_imagem)
-        esperar(0.5)
+        time.sleep(1)
         trocar_a_foto_catalogo(item_da_lista)
-        esperar(0.5)
+        time.sleep(1)
         pos_x += 80
         posicao_imagem = pos_x, pos_y
     else:
         clicar(posicao_imagem)
-        esperar(0.5)
+        time.sleep(1)
         clicar_duas_vezes(posicao_imagem)
-        esperar(0.5)
+        time.sleep(1)
         clicar_duas_vezes(posicao_imagem)
-        esperar(0.5)
+        time.sleep(1)
         trocar_a_foto_catalogo(item_da_lista)
-        esperar(0.5)
+        time.sleep(1)
         pos_x += 80
         posicao_imagem = pos_x, pos_y
 
-# '''
-# escrever_as_informacoes()
+'''
 
-# '''
+# escrever_as_informacoes()
 # programa_trocar_fotos()
 
 alerta('Fim do programa',
