12c9530ed5cbf3ee2717b426ea5e7c45349e1582
imparcialista <lucasaraujoz13@outlook.com>
2024-01-31 00:10:56 -0300

    testando

--- python/flet/captura.py
+++ python/flet/captura.py
@@ -17,26 +17,22 @@ def main(page: ft.Page):
     # page.window_movable = True
     page.window_opacity = 0.90
 
-
     def fechar(e):
         page.window_destroy()
-
 
     x = ft.Text(value=f"Iniciando", color="green")
     y = ft.Text(value=f"Iniciando", color="green")
     sair_btn = ft.ElevatedButton(text="Sair", on_click=fechar)
-
 
     def capturador_1(e):
         page.controls.remove(txt_informativo)
         page.controls.remove(escolha_btn_1)
         page.controls.remove(escolha_btn_2)
         page.controls.append(sair_btn)
-        #page.window_frameless = True
+        # page.window_frameless = True
         page.window_title_bar_hidden = True
         page.window_height = 200
         page.window_width = 110
-
 
         for i in range(0, 60000):
             x = ft.TextField(label='X', value=f'{pyautogui.position()[0]}', width=100)
@@ -48,8 +44,6 @@ def main(page: ft.Page):
             page.controls.remove(y)
             time.sleep(0.01)
         page.controls.append(sair_btn)
-
-
 
     def capturador_2(e):
         page.controls.remove(txt_informativo)
@@ -69,14 +63,14 @@ def main(page: ft.Page):
             time.sleep(0.01)
         page.controls.append(sair_btn)
 
-
     txt_informativo = ft.Text(value='Antes de escolher, deixe o programa em um lugar adequado, '
-                           'não poderá mexer depois, apenas se reinicar o programa')
+                                    'não poderá mexer depois, apenas se reinicar o programa')
     page.add(txt_informativo)
     escolha_btn_1 = ft.ElevatedButton(text="Escolha 1", on_click=capturador_1)
     escolha_btn_2 = ft.ElevatedButton(text="Escolha 2", on_click=capturador_2)
 
     page.add(escolha_btn_1)
     page.add(escolha_btn_2)
+
 
 ft.app(target=main)
