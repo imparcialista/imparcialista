6c89a0870996bbc2c025ab328050191a97d30d62
imparcialista <lucasaraujoz13@outlook.com>
2024-03-24 23:21:46 -0300

    arrumando

--- python/programas/dominios/dicas.py
+++
@@ -1,8 +0,0 @@
-# TODO defina variáveis e funções bem explicadas, jamais coisas muito abreviadas
-# TODO crie sua própria biblioteca, comece por funções que você mais usa de alguma biblioteca, e no lugar de utilizar os nomes deles, você cria a sua função, com a sua definição, mas utilizando o método da biblioteca deles
-# TODO deixe comentários explicando algumas etapas que não pareçam tão claras
-# TODO deixe o seu ambiente a sua cara
-# TODO tenha seu ambiente para testes
-# TODO nunca esqueça que algumas informações são sensíveis e não podem ser mostradas
-
-
